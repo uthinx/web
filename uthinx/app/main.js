61670389b5ef04dac594fec8beef610816e17e81
Zola Richards <zolarichards@Zolas-MacBook-Pro.local>
2014-11-30 18:41:33 -0800

    Changing structure

--- uthinx/app/main.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-/*jslint nomen: true  */
-/*global define: false, window: false, console: false, house: false, locationSetting: false,
- $$: false, document: false, undefined: false, typeof: false, parseInt: false, undefined: false,
- null: false, nomen: false, require: false, requirejs: false
- */
-require(['app'], function(app) {
-    app.initialize();
-});
